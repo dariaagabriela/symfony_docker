f4132a14916de00326817101f48a33251321c56a
dariadraghici <dariadraghici2004@gmail.com>
2025-11-07 11:36:56 +0200

    initial commit

---
+++ app/src/Entity/Quote.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\QuoteRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: QuoteRepository::class)]
+class Quote
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $quote = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $writer = null;
+
+    #[ORM\Column]
+    private ?int $year = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getQuote(): ?string
+    {
+        return $this->quote;
+    }
+
+    public function setQuote(string $quote): static
+    {
+        $this->quote = $quote;
+
+        return $this;
+    }
+
+    public function getWriter(): ?string
+    {
+        return $this->writer;
+    }
+
+    public function setWriter(string $writer): static
+    {
+        $this->writer = $writer;
+
+        return $this;
+    }
+
+    public function getYear(): ?int
+    {
+        return $this->year;
+    }
+
+    public function setYear(int $year): static
+    {
+        $this->year = $year;
+
+        return $this;
+    }
+}
